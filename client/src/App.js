a92a915aa79ca812c1556b8d3d85e4a0b77cee02
GitZiarys <jonathan.bories-ronziere@hetic.net>
2022-03-18 18:27:42 +0100

    Create [Footer] component

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,21 @@ import Header from "./components/header";
 import AnimaideShort from "./components/animaide_short";
 import AssociationsDisplay from "./components/associations_display";
 import AnimaideEngage from "./components/animaide_engagments";
+import AnimaideRecentMissions from "./components/animaide_recent_mission";
+import Footer from "./components/footer";
 
 
 const App = () => {
   return (
       <div className="App">
-       <Header /> 
-       <AnimaideShort />
-       <AssociationsDisplay />
-       <AnimaideEngage />
+        <Header /> 
+
+        <AnimaideShort />
+        <AssociationsDisplay />
+        <AnimaideEngage />
+        <AnimaideRecentMissions />
+
+        <Footer />
       </div>
   );
 }
---
+++ client/src/components/animaide_recent_mission.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const AnimaideRecentMissions = () => {
+  return (
+        <div className="container_recentmissions">
+            <div className="background"></div>
+        </div>
+  );
+}
+
+export default AnimaideRecentMissions;
